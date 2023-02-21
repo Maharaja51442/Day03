24baf536ec770353e97755ca4a797bbf86f370fe
Maharaja51442 <122883257+Maharaja51442@users.noreply.github.com>
2023-02-21 13:18:13 +0530

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,40 @@
+var resume={
+    "name":"Maharaja",
+    "email":"maharaja51442@hotmail.com",
+    "phone":"8248436861",
+    "address": "T.Gudalur,Ariyalur-621730",
+    "summary": "Highly motivated and results-driven Mechanical engineer with 2 years of experience in Gis analyst. Excels in problem-solving and enjoys working in collaborative environments to develop innovative solutions.",
+    "work_experience": [
+      {
+        "position": "GIS Analyst",
+        "employer": "AAA Continuous information",
+        "start_date": "2020-02-20",
+        "end_date": "Present",
+        "location": "chennai,guindy",
+        "description": "Developed and maintained various projects using Micro station and PLS CADD. Collaborated with cross-functional teams to ensure timely delivery of quality products."
+      }
+   ],
+    "education": [
+      {
+        "degree": "Bachelor of Engineering ,Mechanical Engineer",
+        "institution": "Dhanalakshmi Srinivasan college of engineering and Technology, Mamallapuram",
+        "graduation_date": "2019-05-15",
+        "location": "chennai,Mamallapuram"
+      }
+    ],
+    "skills": [
+      {
+        "category": "Programming Languages",
+        "entries": [
+          "JavaScript"
+        ]
+      },
+      {
+        "category": "Tools",
+        "entries": [
+          "Git",
+          "VS Code","PLS CADD"
+        ]
+      }
+    ]
+  }
